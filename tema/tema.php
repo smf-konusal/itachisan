77bba1fcd44bae0381e87f0ad541e2e7cefbb08d
CeeMoo <qdale.123@gmail.com>
2021-09-14 22:37:57 +0300

    up

--- tema/tema.php
+++ tema/tema.php
@@ -4,23 +4,8 @@ namespace HUU;
 
 class Tema {
 	public function yukle(){
-		add_integration_function('integrate_load_profile_fields', self::class.'::addProfileCustomFields', false);
 		add_integration_function('integrate_menu_buttons', self::class.'::addThemeSettingsLinkMenuItem', false);
 		add_integration_function('integrate_setup_profile_context', self::class.'::loadCustomFields', false);
-	}
-
-	/**
-	 * @param $profile_fields
-	 */
-	public static function addProfileCustomFields(&$profile_fields)
-	{
-		global $context, $txt, $options;
-
-		$profile_fields['default_options[bese_dark_mode]'] = array(
-			'type' => 'check',
-			'label' => $txt['bese_dark_mode'],
-			'value' => !empty($options['bese_dark_mode']),
-		);
 	}
 
 	/**
